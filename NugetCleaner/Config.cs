4f4304345f0cc32afd098e186466ec4037517c4f
Julian B <bechti44@googlemail.com>
2023-08-17 13:32:03 +0200

    finalisations

---
+++ NugetCleaner/Config.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+
+namespace NugetCleaner
+{
+    internal static class Config
+    {
+        private const string ConfigFilePath = "NugetCleaner.cfg";
+
+        static Config()
+        {
+            if (File.Exists(ConfigFilePath))
+            {
+                LoadConfig();
+            }
+            else
+            {
+                WriteDefaultConfig();
+            }
+        }
+        public static Settings Settings { get; set; }
+
+        private static void LoadConfig()
+        {
+            var jsonData = File.ReadAllText(ConfigFilePath);
+            var configObj = JsonSerializer.Deserialize<Settings>(jsonData);
+
+            if (configObj != null)
+            {
+                Settings = configObj;
+            }
+        }
+
+        private static void WriteDefaultConfig()
+        {
+            Settings = new Settings();
+            JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping // For better readability
+            };
+
+            var defaultConfigContent = JsonSerializer.Serialize(Settings, jsonSerializerOptions);
+            File.WriteAllText(ConfigFilePath, defaultConfigContent);
+        }
+    }
+    public class Settings
+    {
+        public string MoveToSubdirs_Doc { get; set; } = "If set to true, organizes packages in folders";
+        /// <summary>
+        /// If set to true, organizes packages in folders
+        /// </summary>
+        public bool MoveToSubdirs { get; set; } = true;
+
+        public string VersionRevision_Doc { get; set; } =
+            "Defines the extent to which the software should clean" + Environment.NewLine +
+            "Possible values:" + Environment.NewLine +
+            "- major: only keep the highest full versions, eg 1.3.3; 2.0.1; 3.0.0" + Environment.NewLine +
+            "- minor: keep each highest minor, eg 0.0.0; 0.1.3; 0.2.2" + Environment.NewLine +
+            "- build: keep each revision, eg 0.0.0.0; 0.0.1.23; 0.0.2.33";
+        /// <summary>
+        /// Defines the extent to which the software should clean
+        /// </summary>
+        /// <remarks>
+        /// Possible values:<br/>
+        /// - major: only keep the highest full versions, eg 1.3.3; 2.0.1; 3.0.0
+        /// - minor: keep each highest minor, eg 0.0.0; 0.1.3; 0.2.2
+        /// - build: keep each revision, eg 0.0.0.0; 0.0.1.23; 0.0.2.33
+        /// </remarks>
+        public string VersionRevision
+        {
+            get
+            {
+                return _VersionRevision.ToUpperInvariant().Trim();
+            }
+            set
+            {
+                _VersionRevision = value;
+            }
+        } 
+        private string _VersionRevision = "build";
+    }
+}
--- NugetCleaner/Program.cs
+++ NugetCleaner/Program.cs
@@ -1,2 +1,154 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NugetCleaner;
+using System.Reflection;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        var directoryPath = Directory.GetCurrentDirectory();
+
+        // Get all relevant package files
+        (FileInfo[] nupkgs, FileInfo[] snupkgs) packageFiles = GetPackageFiles(directoryPath);
+
+        var nugetPackagesToKeep = FilesToKeep(packageFiles.nupkgs);
+        var snugetPackagesToKeep = FilesToKeep(packageFiles.snupkgs);
+
+        DeleteOldVersions(packageFiles.nupkgs, nugetPackagesToKeep);
+        DeleteOldVersions(packageFiles.snupkgs, snugetPackagesToKeep);
+
+        Console.WriteLine("Deduplication completed!");
+    }
+
+    private static (FileInfo[] nupkgs, FileInfo[] snupkgs)GetPackageFiles(string directoryPath)
+    {
+        EnumerationOptions options = new EnumerationOptions();
+        options.RecurseSubdirectories = true;
+        // pull files
+        var nupkgFiles = Directory.GetFiles(directoryPath, "*.nupkg", options);
+        var snupkgFiles = Directory.GetFiles(directoryPath, "*.snupkg", options);
+        // convert to fileinfo array
+        FileInfo[] nupkgs = new FileInfo[nupkgFiles.Length];
+        for (int i = 0; i < nupkgFiles.Length; i++)
+        {
+            nupkgs[i] = new FileInfo(nupkgFiles[i]);
+        }
+        FileInfo[] snupkgs = new FileInfo[snupkgFiles.Length];
+        for (int i = 0; i < snupkgFiles.Length; i++)
+        {
+            snupkgs[i] = new FileInfo(snupkgFiles[i]);
+        }
+        return (nupkgs, snupkgs);
+    }
+    private static StringBuilder stringBuider = new StringBuilder();
+    private static (string packageName, string packageIdentifier, Version version) GetpackageName(FileInfo file)
+    {
+        string name = Path.GetFileNameWithoutExtension(file.Name);
+        // read version (inverted)
+        List<char> versionCharArray = new List<char>();
+        for (int i = name.Length - 1; i >= 0; i--)
+        {
+            char c = name[i];
+            if (char.IsDigit(c) || c == '.' || i == name.Length - 1 || c == '-')
+            {
+                versionCharArray.Add(c);
+            }
+            else
+            {
+                if (versionCharArray.Last() == '.')
+                {
+                    versionCharArray.RemoveAt(versionCharArray.Count - 1);
+                }
+                break;
+            }
+        }
+        // correct inversion
+        versionCharArray.Reverse();
+        string versionString = string.Join("", versionCharArray);
+        // trim version
+        string[] versionParts = versionString.Split('-')[0].Split('.');
+        string trimmedVersion = string.Join('.', versionParts.Take(4));
+        // convert to version
+        Version packageVersion = Version.Parse(trimmedVersion);
+        // get packageName
+        string packageName = name.Substring(0, (name.Length - versionString.Length)-1);
+        // get package version identifier
+        stringBuider.Clear();
+        stringBuider.Append(packageName);
+        stringBuider.Append(packageVersion.Major);
+        if (Config.Settings.VersionRevision == "MINOR"
+            || Config.Settings.VersionRevision == "BUILD"
+            || Config.Settings.VersionRevision == "REVISION")
+        {
+            stringBuider.Append('.');
+            stringBuider.Append(packageVersion.Minor);
+        }
+        if (Config.Settings.VersionRevision == "BUILD"
+            || Config.Settings.VersionRevision == "REVISION")
+        {
+            stringBuider.Append('.');
+            stringBuider.Append(packageVersion.Build);
+        }
+        string packageIdentifier = stringBuider.ToString();
+        return (packageName, packageIdentifier, packageVersion);
+    }
+    /// <summary>
+    /// enumerates which files to keep
+    /// </summary>
+    /// <param name="packageFiles"></param>
+    /// <returns></returns>
+    private static HashSet<FileInfo> FilesToKeep(FileInfo[] packageFiles)
+    {
+        Dictionary<string, Version> packageVersions = new Dictionary<string, Version>();
+        Dictionary<string, FileInfo> packages = new Dictionary<string, FileInfo>();
+
+        
+        foreach(FileInfo file in packageFiles)
+        {
+            (string packageName, string packageIdentifier, Version version) packageInfo = GetpackageName(file);
+            if (packageVersions.TryGetValue(packageInfo.packageIdentifier, out Version lastVersion))
+            {
+                if (packageInfo.version > lastVersion)
+                {
+                    packageVersions[packageInfo.packageIdentifier] = packageInfo.version;
+                    packages[packageInfo.packageIdentifier] = file;
+                }
+            }
+            else
+            {
+                packageVersions[packageInfo.packageIdentifier] = packageInfo.version;
+                packages[packageInfo.packageIdentifier] = file;
+            }
+        }
+        return packages.Values.ToHashSet();
+    }
+    /// <summary>
+    /// deletes and reorganizes the old package files
+    /// </summary>
+    /// <param name="packageFiles"></param>
+    /// <param name="packagesToKeep"></param>
+    private static void DeleteOldVersions(FileInfo[] packageFiles, HashSet<FileInfo> packagesToKeep)
+    {
+        foreach (FileInfo oldPackage in packageFiles.Except(packagesToKeep))
+        {
+            oldPackage.Delete();
+            Console.WriteLine($"Deleting {oldPackage}...");
+        }
+        if (Config.Settings.MoveToSubdirs)
+        {
+            foreach (FileInfo package in packagesToKeep)
+            {
+                (string packageName, string packageIdentifier, Version version) packageInfo = GetpackageName(package);
+                DirectoryInfo targetDir = new DirectoryInfo(packageInfo.packageName);
+                if (!targetDir.Exists)
+                {
+                    targetDir.Create();
+                }
+                string targetPath = Path.Combine(targetDir.FullName, package.Name);
+                if (package.FullName != targetPath)
+                {
+                    package.MoveTo(targetPath, overwrite: true);
+                }
+            }
+        }
+    }
+}
